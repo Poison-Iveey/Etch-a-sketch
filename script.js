b54ebc1dbb840e21951d9730317089fed22fc3f8
Iveey <yvettechristine245@gmail.com>
2024-06-10 10:09:48 +0300

    The code in this files don't work ,they are theoretically correct but not practical.

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 
-const rows = 16;
-const columns = 16;
 
 
-const gridContainer = document.getElementById("myGrid");
-for (let i = 1; i < 257; i++){
-        const gridItem = document.createElement('div');
+let gridContainer = document.getElementById("myGrid");
+
+for (let i = 0; i < 256; i++){   // to loop from 0 to 255 making it 256 grids in total
+        let gridItem = document.createElement('div');
         gridItem.className = 'grid-item';
-        gridContainer.appendChild(gridItem);
+        gridContainer.appendChild(gridItem);}
         
-      }
-    
 
-// In the above, i have specified the grid’s number of rows and columns. 
-//then use a nested loop to create grid items dynamically. 
-//Each grid item is an <div> element with the class “grid-item.”
+
+        let gridItems = document.getElementsByClassName('grid-item');
+        for (let i = 0; i < gridItems.length; i++) {
+            gridItems[i].addEventListener("mouseover", function() {
+                this.style.background = "#ffd66b";
+            });
+        }
+
+
+
+     function myFunction() {
+            let new_grid = prompt('How many squares per side do you want? Please enter a number <= 100:');
+            new_grid = parseInt(new_grid, 10);
+        
+            if (isNaN(new_grid) || new_grid > 100) {
+                alert("Input a number below or equal to 100!");
+            } else {
+                // Clear any existing grid
+                let gridContainer = document.getElementById("myGrid");
+                gridContainer.removeChild(gridItems)
+        
+                // Create new grid
+                for (let i = 0; i < new_grid; i++) {
+                    for (let j = 0; j < new_grid; j++) {
+                        let square = document.createElement("div");
+                        square.classList.add("grid-square");
+                        square.addEventListener("click", function() {
+                            alert("Square clicked!");
+                        });
+                        gridContainer.appendChild(square);
+                    }
+                   
+                }
+            }
+        }
+
+
+        let square = document.getElementsByClassName('grid-square');
+        for (let i = 0; i < square.length; i++) {
+            square[i].addEventListener("mouseover", function() {
+                this.style.background = "#ffd66b";
+            });
+        } 
+        */
+
+  
+    
+  
+
